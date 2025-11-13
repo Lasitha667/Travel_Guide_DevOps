ca72e0e892d9acb22ac8e4086f399a4558413fa4
Lasitha667 <klasitha2089@gmail.com>
2025-11-14 01:35:41 +0530

    6 commit

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 
 import SignUp from "./assets/Components//Signup/Signup";
 import Login from "./assets/Components/Login/Login";
+import Home from "./assets/Components/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
